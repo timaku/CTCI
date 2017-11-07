7a4f2ae9f5e3449abffeb3fc003de5db99e4df98
Tim <timrocks@uw.edu>
2017-11-06 18:38:47 -0800

    First commit

---
+++ calculateHarmonicNum/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BigDecimal answer = new BigDecimal(0);
+
+        for(int i = 1; i <=95 ; i++) {
+            answer.add()
+        }
+
+
+    }
+}
